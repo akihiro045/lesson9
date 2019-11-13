786e107fb14efe6643e87c307f9fb61690988b4d
akina <g1727045@st.t-kougei.ac.jp>
2019-11-13 17:59:18 +0900

    後日再びやる予定

---
+++ 1_trail_renderer/trail_renderer/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    GameObject sword;
+
+    void Start()
+    {
+        sword = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += new Vector3(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position += new Vector3(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(-speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(speed, 0f, 0f);
+        }
+
+        //transform.Rotate(new Vector3(5, 0, 0));
+        Swipe();
+    }
+
+    void Swipe()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 target_pos = ray.GetPoint(5.0f);
+
+            sword.transform.LookAt(target_pos);
+        }
+    }
+}
---
+++ 1_trail_renderer/trail_renderer/Assets/TrailController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TrailController : MonoBehaviour
+{
+    private const int FRAME_MAX = 10;
+    private List<Vector3> points0 = new List<Vector3>();//根本
+    private List<Vector3> points1 = new List<Vector3>();//先端
+
+    private Mesh mesh;
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Update()
+    {
+        if (FRAME_MAX <= points0.Count)
+        {
+            points0.RemoveAt(0);
+            points1.RemoveAt(0);
+        }
+
+        points0.Add(transform.position);
+        points1.Add(transform.TransformPoint(new Vector3(0.0f, 1.0f, 0.0f)));
+
+        if (2 <= points0.Count)
+        {
+            CreateMesh();
+        }
+    }
+
+    private void CreateMesh()
+    {
+        mesh.Clear();//メッシュのクリア
+
+        //メモリの確保
+        int n = points0.Count;
+        Vector3[] vertexArray = new Vector3[2 * n];
+        Vector2[] uvArray = new Vector2[2 * n];
+        int[] indexArray = new int[6 * (n - 1)];
+
+        int idv = 0, idi = 0;
+        float duv = 1.0f / ((float)n - 1.0f);
+        for (int i = 0; i < n; i++)
+        {
+            //超転座標
+            vertexArray[idv + 0] = transform.InverseTransformPoint(points0[i]);
+            vertexArray[idv + 1] = transform.InverseTransformPoint(points1[i]);
+
+            //UV座標
+            uvArray[idv + 0].x =
+            uvArray[idv + 1].x = 1.0f - (float)i;
+            uvArray[idv + 0].y = 0.0f;
+            uvArray[idv + 1].y = 1.0f;
+
+            //インデックス
+            if (i == n - 1) break;//最後の辺では作らない
+            indexArray[idi + 0] = idv;
+            indexArray[idi + 1] = idv + 1;
+            indexArray[idi + 2] = idv + 2;
+            indexArray[idi + 3] = idv + 2;
+            indexArray[idi + 4] = idv + 1;
+            indexArray[idi + 5] = idv + 3;
+
+            idv += 2;
+            idi += 6;
+        }
+
+        mesh.vertices = vertexArray;
+        mesh.uv = uvArray;
+        mesh.triangles = indexArray;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+}
